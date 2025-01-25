9e90fc6a5acab38d800d0de7ed7ce5ffd92dd346
Michael262626 <dikandumichael@gmail.com>
2025-01-26 00:23:06 +0100

    Paystack set up done

--- src/lib/User.ts
+++ src/lib/User.ts
@@ -2,7 +2,7 @@ import { PrismaClient, Prisma } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-export async function createUser(data: Prisma.UserCreateInput) {
+export async function createUser(data: any) {
   return prisma.user.create({ data });
 }
 
@@ -14,7 +14,7 @@ export async function getUserByEmail(email: string) {
   return prisma.user.findUnique({ where: { email } });
 }
 
-export async function updateUser(id: string, data: Prisma.UserUpdateInput) {
+export async function updateUser(id: string, data: any) {
   return prisma.user.update({
     where: { id },
     data,
