81b59336c6fadde08b3a50b413abf0f9f2f0cbbe
Michael262626 <dikandumichael@gmail.com>
2025-01-25 23:36:07 +0100

    Paystack set up done

--- src/lib/User.ts
+++ src/lib/User.ts
@@ -2,34 +2,26 @@ import { PrismaClient, Prisma } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-// Define type explicitly for data parameter
-export async function createUser(
-  data: Prisma.UserCreateInput
-): Promise<Prisma.User> {
+export async function createUser(data: Prisma.UserCreateInput) {
   return prisma.user.create({ data });
 }
 
-export async function getUserById(id: string): Promise<Prisma.User | null> {
+export async function getUserById(id: string) {
   return prisma.user.findUnique({ where: { id } });
 }
 
-export async function getUserByEmail(
-  email: string
-): Promise<Prisma.User | null> {
+export async function getUserByEmail(email: string) {
   return prisma.user.findUnique({ where: { email } });
 }
 
-export async function updateUser(
-  id: string,
-  data: Prisma.UserUpdateInput
-): Promise<Prisma.User> {
+export async function updateUser(id: string, data: Prisma.UserUpdateInput) {
   return prisma.user.update({
     where: { id },
     data,
   });
 }
 
-export async function deleteUser(id: string): Promise<Prisma.User> {
+export async function deleteUser(id: string) {
   return prisma.user.delete({ where: { id } });
 }
 
