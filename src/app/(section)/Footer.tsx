b3452714ff7cbaf6c7319060fc10034022435a74
Michael262626 <dikandumichael@gmail.com>
2025-01-30 10:30:59 +0100

    Commit

--- src/app/(section)/Footer.tsx
+++ src/app/(section)/Footer.tsx
@@ -55,7 +55,7 @@ const Footer = () => {
           </div>  
         </div>  
       </div>  
-      <div className="flex flex-col p-6 md:flex-row mb-2 md:mt-6 gap-2 justify-between items-center w-full bg-black text-white"> 
+      <div className="flex flex-col p-6 md:flex-row -mb-12 gap-2 justify-between items-center w-full bg-black text-white"> 
         <h2 className="text-gray-400">@2025</h2>   
         <Image src={logo} width={150} height={20} alt="Logo" className="md:w-[250px] md:h-auto" />  
         <div className="flex gap-4 text-gray-400">  
