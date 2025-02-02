e598bf83fc7fb5064f4576a1b2c429f79df0196c
Michael262626 <dikandumichael@gmail.com>
2025-02-02 20:21:24 +0100

    Commit

--- src/app/(pages)/sign-up/SignUp.tsx
+++ src/app/(pages)/sign-up/SignUp.tsx
@@ -91,7 +91,7 @@ const SignUpPage: React.FC = () => {
             value={formData.email}
             onChange={handleInputChange}
             required
-            className="mt-1 block w-full text-black px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+            className="mt-1  block w-full text-black px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
           />
         </div>
 
